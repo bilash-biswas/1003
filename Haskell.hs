ebbf82f3a849f0bf4e4319643e08dc544cad7f0b
bilash-biswas <67077735+bilash-biswas@users.noreply.github.com>
2020-11-17 14:55:23 +0600

    Update and rename Haskell to Haskell.hs

--- Haskell.hs
+++ Haskell.hs
@@ -2,4 +2,4 @@ main :: IO ()
 main = do
         a <- readLn :: IO Int
         b <- readLn :: IO Int
-        putStrLn $ id ("SOMA = " ++ show(a+b))
+        putStrLn $ id ("SOMA = " ++ show(a + b))
